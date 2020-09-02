31888ffff01596ee3f00bffc8e49c8fe899b4109
Michael Hearn <git@mikehearn.net>
2020-09-02 09:56:26 -0400

    Update init and kitty conf

--- init.sh
+++ init.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env /bin/bash
 
+rm ~/.hammerspoon/init.lua
+rm ~/.config/karabiner/karabiner.json
+rm ~/.config/kitty
+rm ~/.mackup.cfg
+
 stow -t $HOME hammerspoon
 stow -t $HOME karabiner
 stow -t $HOME kitty
